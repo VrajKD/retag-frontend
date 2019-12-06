34c3bcc07bf7d0c60c0e05ce0d4c04288532b049
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-12-07 00:16:33 +0530

    TagList Update finally done

--- src/components/TagList/TagList.js
+++ src/components/TagList/TagList.js
@@ -5,7 +5,7 @@ import ListProduct from './ListProduct/ListProduct'
 class TagList extends React.Component {
     render() {
         return (
-            <div className="fl w-70 dib ml5 mr2 mv5">
+            <div className="fl w-90 dib ml5 mr2 mv5">
                 <table className="list-table tc dib">
                     <tr className="white bg-dark-gray f5 tc ">
                         <th>TAG ID</th>
