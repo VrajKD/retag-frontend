6d52059cc550f25dbe293e17d673b98d85b5b081
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-12-06 19:38:25 +0530

    Updates

---
+++ precache-manifest.fbceec56815599aa48898d17665367ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534f070533e6bfc239857f5448a728f4",
+    "url": "/retag-frontend/index.html"
+  },
+  {
+    "revision": "79183adcb1a3bbd45038",
+    "url": "/retag-frontend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6b1479a242ac1c79ce8d",
+    "url": "/retag-frontend/static/css/main.305114ab.chunk.css"
+  },
+  {
+    "revision": "79183adcb1a3bbd45038",
+    "url": "/retag-frontend/static/js/2.911b6bd3.chunk.js"
+  },
+  {
+    "revision": "6b1479a242ac1c79ce8d",
+    "url": "/retag-frontend/static/js/main.c85d65e0.chunk.js"
+  },
+  {
+    "revision": "2633d58fc290c0403093",
+    "url": "/retag-frontend/static/js/runtime-main.c6f2a553.js"
+  }
+]);
